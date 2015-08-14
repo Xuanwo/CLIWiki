20907366785b63f9e99809871a1eaa4e0cb492e8
Xuanwo <xuanwo.cn@gmail.com>
2015-08-15 07:53:26 +0800

    seems like the git was upgrade to 2.0

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    cd output
-    echo "cli.xuanwo.org" >> CNAME
-    DATE=`date +%Y-%m-%d`
-    TIME=`date +%H:%M:%S`
-    git add --all
+    git add --all .
     git commit -m "CLI wiki updated at $DATE $TIME"
     git push --force
 fi
