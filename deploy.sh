9ca45f4fe011912a2a9f235d622e798c668dfad8
Xuanwo <xuanwo.cn@gmail.com>
2015-08-15 08:12:23 +0800

    last try

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+    cd output
+    echo "cli.xuanwo.org" >> CNAME
+    DATE=`date +%Y-%m-%d`
+    TIME=`date +%H:%M:%S`
     git add --all .
     git commit -m "CLI wiki updated at $DATE $TIME"
     git push --force
