f51cc88e80d42370fff6dc79f9e328b8fd42a0af
Xuanwo <xuanwo.cn@gmail.com>
2016-02-05 22:33:04 +0800

    fix CNAME

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     cd output
-    echo "cli.xuanwo.org" >> CNAME
+    echo "cli.xuanwo.org" > CNAME
     DATE=`date +%Y-%m-%d`
     TIME=`date +%H:%M:%S`
     git add --all .
